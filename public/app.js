771decacd7e1855c7b8e6a6f5095344c0e35f433
freetimezp <kraftukrnet@ukr.net>
2022-08-13 17:03:13 +0300

    02.Js-small-game-01. Rock-paper-scissors.

---
+++ public/app.js
@@ -0,0 +1,61 @@
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+
+const possibleChoices = document.querySelectorAll('button');
+
+let userChoice = '';
+let computerChoice = '';
+let result = '';
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+
+    generateComputerCoice();
+    getResult();
+}));
+
+function generateComputerCoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    if(randomNumber === 1) {
+        computerChoice = 'rock';
+    }
+    if(randomNumber === 2) {
+        computerChoice = 'scissors';
+    }
+    if(randomNumber === 3) {
+        computerChoice = 'paper';
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+    if(computerChoice === userChoice) {
+        result = "Its a draw!";
+    }
+
+    if(computerChoice === 'rock' && userChoice === 'paper') {
+        result = "YOU WIN!";
+    }
+    if(computerChoice === 'rock' && userChoice === 'scissors') {
+        result = "YOU LOSE!";
+    }
+
+    if(computerChoice === 'paper' && userChoice === 'rock') {
+        result = "YOU LOSE!";
+    }
+    if(computerChoice === 'paper' && userChoice === 'scissors') {
+        result = "YOU WIN!";
+    }
+
+    if(computerChoice === 'scissors' && userChoice === 'paper') {
+        result = "YOU LOSE!";
+    }
+    if(computerChoice === 'scissors' && userChoice === 'rock') {
+        result = "YOU WIN!";
+    }
+
+    resultDisplay.innerHTML = result;
+}
